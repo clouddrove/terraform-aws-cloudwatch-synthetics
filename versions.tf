91e5156a59cbcb8c4e9e99616a3da8ed312d5e55
Chetan Kesare <chetan.kesare@clouddrove.com>
2026-05-06 01:38:10 +0530

    fix: changed provider meta

--- versions.tf
+++ versions.tf
@@ -9,6 +9,6 @@ terraform {
   }
 
   provider_meta "aws" {
-    module_name = "clouddrove/terraform-aws-cloudwatch-synthetics"
+    user_agent = ["github.com/clouddrove/terraform-aws-cloudwatch-synthetics"]
   }
 }
